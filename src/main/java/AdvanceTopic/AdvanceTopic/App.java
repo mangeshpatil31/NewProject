25e6f548cb86c0a47ade42ff01d45a2cafb96a68
TechMangeshPAtil <TechMangeshPatil>
2023-08-13 00:19:06 +0530

    initial commit

---
+++ src/main/java/AdvanceTopic/AdvanceTopic/App.java
@@ -0,0 +1,13 @@
+package AdvanceTopic.AdvanceTopic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/it/allurereport/AllureReport.java
@@ -0,0 +1,34 @@
+package com.it.allurereport;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+public class AllureReport {
+
+	WebDriver driver;
+	
+	
+	@Test( )
+	public void pageLogin() {
+	
+		System.setProperty("WebDriver.Chrome.driver", "D:\\Drivers\\chromedriver_win3/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+		      
+		   driver.findElement(By.xpath("//a[@class='ico-login']")).click();
+           driver.findElement(By.xpath("//input[@class='email']")).sendKeys("techmangeshpatil31@gmail.com");
+           driver.findElement(By.xpath("//input[@class='password']")).sendKeys("8888459441");
+           driver.findElement(By.xpath("//button[@class='button-1 login-button']")).click();      
+ 
+		
+	}	
+	
+	@Test
+	public void closeapp() {
+		driver.close();
+	}
+}
